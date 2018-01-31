495a8c02215bf2dd3fde143eb66ae9ebb23381a6
has1g15 <hannah.short1@btinternet.com>
2018-01-31 08:16:57 +0000

    Update SlopeOne.java

--- src/SlopeOne.java
+++ src/SlopeOne.java
@@ -1,105 +1,140 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import com.almworks.sqlite4java.SQLiteException;
+import com.almworks.sqlite4java.SQLiteStatement;
+import com.google.common.collect.Multimap;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.*;
 
 /**
  * Created by Hannah on 27/10/2017.
  */
 public class SlopeOne {
 
-    private float[] predictions;
     private DataHandler dataHandler;
-    private HashMap<Integer, HashMap<Integer, Float>> data;
-    private int entries;
+    private HashMap<Integer, HashMap<Integer, Float>> data, userData;
+    private Multimap<Integer, Integer> testData;
+    private HashMap<Integer, Float> averages = new HashMap<>();
 
     public SlopeOne()
     {
         dataHandler = new DataHandler();
         loadData();
-        entries = dataHandler.getEntryNo();
-        calculateItemDiff();
+        makePredictions();
         dataHandler.finish();
     }
 
     public void loadData()
     {
-        data = dataHandler.loadRatings("slopeOne");
+        data = dataHandler.loadItemData();
+        userData = dataHandler.loadUserData();
+        testData = dataHandler.loadTestData();
+        calculateAverages();
     }
 
-
-    public float[] getPredictions()
+    //iterate through test data set, pass users and items to calc prediction then add data to predictions hashmap
+    public void makePredictions()
     {
-        return predictions;
+        //dataHandler.createPredictionsTable();
+        int item;
+        float prediction;
+        for (Map.Entry<Integer,Integer> entry: testData.entries())
+        {
+            item = entry.getValue();
+            prediction = calcPrediction(entry.getKey(), item);
+            if (prediction > 10.49)
+            {
+                prediction = 10;
+            }
+            if (prediction < 0.5)
+            {
+                prediction = 1;
+            }
+            System.out.println("Prediction: " + prediction);
+            dataHandler.updatePredictionsTable(entry.getKey(), item, Math.round(prediction));
+        }
     }
 
-
-    public void calculateItemDiff()
+    //calculate prediction - average item rating for user + average of differences
+    public float calcPrediction(int user, int item)
     {
-        dataHandler.createDifferencesTable("differences");
-        float difference = 1;
+        float avg = averages.get(user);
+        float totalDifference = 0;
+        int count = 0;
+        float difference = calculateItemDiffAvg(item);
+        {
+            totalDifference = totalDifference + difference;
+            count++;
+        }
+        float diffAvg = totalDifference/count;
+        return avg + difference;
+    }
+
+    public float calculateItemDiffAvg(int item)
+    {
+        //List<Float> itemDifferences = new ArrayList<>();
+        //dataHandler.createDifferencesTable("Differences");
+        float itemDiffTotal = 0;
+        int count;
+        int counter = 0;
         HashMap<Integer, Float> itemIRatings, itemJRatings;
         List<Integer> userGroupx, userGroupy;
-        List<Float> differences;
+        float differences;
         int xUser, yUser;
         float rating1, rating2;
 
-        for (int i = 1; i < entries; i++)
+        itemIRatings = data.get(item);
+
+        for (int i = 1; i <= data.size(); i++)
         {
-            System.out.println("Item: " + i);
-            for (int j = i+1; j <= entries; j++)
+            itemJRatings = data.get(i);
+            differences = 0;
+            count = 0;
+
+            if (itemIRatings != null && itemJRatings != null)
             {
-                System.out.println("Item " + j);
-                itemIRatings = data.get(i);
-                itemJRatings = data.get(j);
-                differences = new ArrayList<>();
+                userGroupx = new ArrayList<>(itemIRatings.keySet());
+                userGroupy = new ArrayList<>(itemJRatings.keySet());
 
-                if (itemIRatings != null && itemJRatings != null)
+                for (int x = 0; x < itemIRatings.size(); x++)
                 {
-                    userGroupx = new ArrayList<>(itemIRatings.keySet());
-                    userGroupy = new ArrayList<>(itemJRatings.keySet());
+                    xUser = userGroupx.get(x);
 
-                    for (int x = 0; x < itemIRatings.size(); x++)
+                    for (int y = 0; y < itemJRatings.size(); y++)
                     {
-                        xUser = userGroupx.get(x);
-                        //System.out.println("X Value: " + x);
+                        yUser = userGroupy.get(y);
 
-                        for (int y = 0; y < itemJRatings.size(); y++)
+                        if (xUser == yUser)
                         {
-                            yUser = userGroupy.get(y);
-                            //System.out.println("Y Value: " + y);
-
-                            if (xUser == yUser)
-                            {
-                                rating1 = itemIRatings.get(xUser);
-                                rating2 = itemJRatings.get(yUser);
-                                System.out.println("User x: " + xUser);
-                                System.out.println("User y: " + yUser);
-                                System.out.println("Item: " + i);
-                                System.out.println("Item: " + j);
-                                System.out.println(rating1);
-                                System.out.println(rating2);
-                                System.out.println(rating1 - rating2);
-                                differences.add(rating1 - rating2);
-                            }
+                            rating1 = itemIRatings.get(xUser);
+                            rating2 = itemJRatings.get(yUser);
+                            differences = differences + (rating1 - rating2);
+                            count++;
                         }
                     }
-                    if (differences.size() > 1)
+                }
+                if (count > 1) {
+                    itemDiffTotal = itemDiffTotal + (differences / count);
+                }
+                counter++;
+                    /*if (differences.size() > 1)
                     {
-                        System.out.println(calculateAvgItemDiff(differences));
                         difference = calculateAvgItemDiff(differences);
-                        System.out.println("**********");
+
+                        //itemDifferences.add(difference);
                     }
                     else if (differences.size() == 1)
                     {
                         difference = differences.get(0);
-                    }
-                    dataHandler.updateDifferencesTable(i, j, difference);
-                }
+                        //itemDifferences.add(difference);
+
+                    }*/
             }
         }
+        return itemDiffTotal/counter;
     }
 
-    public float calculateAvgItemDiff(List<Float> itemRatingDifferences)
+    /*public float calculateAvgItemDiff(List<Float> itemRatingDifferences)
     {
         float total = 0;
         for(Float difference: itemRatingDifferences)
@@ -107,5 +142,33 @@ public class SlopeOne {
             total = total + difference;
         }
         return total/itemRatingDifferences.size();
+    }*/
+
+    /*public float calcAvgUserRating(int user)
+    {
+        return averages.get(user);
+        /*float total = 0;
+        int count = 0;
+        float avg;
+
+        for (Float rating: userData.get(user).values())
+        {
+            total = total + rating;
+            count++;
+        }
+        avg = total/count;
+        return avg;*/
+    //}
+
+    public void calculateAverages () {
+        try {
+            SQLiteStatement statement = dataHandler.c.prepare("select userID, cast(avg(rating) as real) from trainingUserBased group by userID;");
+            while (statement.step()) {
+                averages.put(statement.columnInt(0), (float) statement.columnInt(1));
+                System.out.println(statement.columnInt(0) + " " + (float) statement.columnInt(1));
+            }
+        } catch (SQLiteException e) {
+            e.printStackTrace();
+        }
     }
 }
